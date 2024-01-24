4041b33feda1bd82940f9868c0094a325f1f9068
Machurui <62929512+Machurui@users.noreply.github.com>
2024-01-24 15:09:44 +0100

    Last push

--- src/projet/main.go
+++ src/projet/main.go
@@ -56,6 +56,57 @@ func main() {
 		} else if os.Args[1] == "-help" {
 			// Liste de toutes les commandes disponibles
 			fmt.Println("C'est en cours")
+		} else if os.Args[1] == "file" {
+			// Vérifie si le nombre d'argument est supérieur à 2
+			if len(os.Args) > 2 {
+
+				// CRUD
+				if os.Args[2] == "create" {
+					// Vérifie si le nombre d'argument est supérieur à 3
+					if len(os.Args) > 3 {
+						// Créer un fichier
+						createFile(os.Args[3])
+					} else {
+						fmt.Println("Le chemin est vide.")
+					}
+				} else if os.Args[2] == "read" {
+					// Vérifie si le nombre d'argument est supérieur à 3
+					if len(os.Args) > 3 {
+						// Lire le fichier
+						readFile(os.Args[3])
+					} else {
+						fmt.Println("Le chemin est vide.")
+					}
+				} else if os.Args[2] == "updatename" {
+					// Vérifie si le nombre d'argument est supérieur à 4
+					if len(os.Args) > 4 {
+						// Met à jour le nom du dossier
+						updateNameFile(os.Args[3], os.Args[4])
+					} else {
+						fmt.Println("Le chemin est vide.")
+					}
+				} else if os.Args[2] == "updatetext" {
+					// Vérifie si le nombre d'argument est supérieur à 4
+					if len(os.Args) > 4 {
+						// Met à jour le nom du dossier
+						updateTextFile(os.Args[3], os.Args[4])
+					} else {
+						fmt.Println("Le chemin est vide.")
+					}
+				} else if os.Args[2] == "delete" {
+					// Vérifie si le nombre d'argument est supérieur à 3
+					if len(os.Args) > 3 {
+						// Delete un dossier
+						deleteFile(os.Args[3])
+					} else {
+						fmt.Println("Le chemin est vide.")
+					}
+				} else {
+					fmt.Println("Aucune commande ne correspond à votre saisie.")
+				}
+			} else {
+				fmt.Println("Il est nécessaire de saisir un argument à la suite de la commande dir.")
+			}
 		}
 	} else {
 		fmt.Println("Exécutez la commande \"-help\" si vous n'êtes pas familier avec les instructions disponibles.")
@@ -132,4 +183,60 @@ func deleteFolder(name string) {
 			fmt.Println("Le dossier, ainsi que toutes les données qu'il contenait, ont été intégralement supprimés.")
 		}
 	}
+}
+
+func createFile(name string) {
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + name
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		file, err := os.Create(filePath)
+		if err != nil {
+			fmt.Println("Impossible de créer le fichier:", err)
+			return
+		}
+		defer file.Close()
+		fmt.Println("Fichier créé:", filePath)
+	} else {
+		fmt.Println("Le fichier existe déjà.")
+	}
+}
+
+func readFile(name string) {
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + name
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Impossible de lire le fichier:", err)
+		return
+	}
+	fmt.Println("Contenu du fichier:", string(data))
+}
+
+func updateTextFile(name string, data string) {
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + name
+	err := os.WriteFile(filePath, []byte(data), 0644)
+	if err != nil {
+		fmt.Println("Impossible de mettre à jour le fichier:", err)
+		return
+	}
+	fmt.Println("Fichier mis à jour.")
+}
+
+func updateNameFile(oldName string, newName string) {
+	oldPath := "C:\\GoEstiamProjet\\src\\data\\" + oldName
+	newPath := "C:\\GoEstiamProjet\\src\\data\\" + newName
+	err := os.Rename(oldPath, newPath)
+	if err != nil {
+		fmt.Println("Impossible de mettre à jour le nom fichier:", err)
+		return
+	}
+	fmt.Println("Nom fichier mis à jour.")
+}
+
+func deleteFile(name string) {
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + name
+	err := os.Remove(filePath)
+	if err != nil {
+		fmt.Println("Impossible de supprimer le fichier:", err)
+		return
+	}
+	fmt.Println("Fichier supprimé.")
 }
