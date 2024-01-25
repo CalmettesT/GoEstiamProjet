09b0f705bbd46a50b9f90a8b598b7e6970e93527
Machurui <62929512+Machurui@users.noreply.github.com>
2024-01-25 14:14:29 +0100

    Add switch gestion main.go

--- src/projet/go.mod
+++ src/projet/go.mod
@@ -10,3 +10,5 @@ require (
 	projet/dossiers v0.0.0-00010101000000-000000000000
 	projet/fichiers v0.0.0-00010101000000-000000000000
 )
+
+require golang.org/x/text v0.14.0 // indirect
--- src/projet/main.go
+++ src/projet/main.go
@@ -8,15 +8,16 @@ import (
 )
 
 func main() {
-
 	// Dossier
 	if len(os.Args) > 1 {
-		if os.Args[1] == "dir" {
+		switch os.Args[1] {
+
+		case "dir":
 			// Vérifie si le nombre d'argument est supérieur à 2
 			if len(os.Args) > 2 {
 
-				// CRUD
-				if os.Args[2] == "create" {
+				switch os.Args[2] {
+				case "create":
 					// Vérifie si le nombre d'argument est supérieur à 3
 					if len(os.Args) > 3 {
 						// Créer un dossier
@@ -24,7 +25,8 @@ func main() {
 					} else {
 						fmt.Println("Le chemin est vide.")
 					}
-				} else if os.Args[2] == "read" {
+
+				case "read":
 					// Vérifie si le nombre d'argument est supérieur à 3
 					if len(os.Args) > 3 {
 						// Lire le dossier
@@ -32,7 +34,8 @@ func main() {
 					} else {
 						fmt.Println("Le chemin est vide.")
 					}
-				} else if os.Args[2] == "update" {
+
+				case "update":
 					// Vérifie si le nombre d'argument est supérieur à 4
 					if len(os.Args) > 4 {
 						// Met à jour le nom du dossier
@@ -40,7 +43,8 @@ func main() {
 					} else {
 						fmt.Println("Le chemin est vide.")
 					}
-				} else if os.Args[2] == "delete" {
+
+				case "delete":
 					// Vérifie si le nombre d'argument est supérieur à 3
 					if len(os.Args) > 3 {
 						// Delete un dossier
@@ -48,21 +52,20 @@ func main() {
 					} else {
 						fmt.Println("Le chemin est vide.")
 					}
-				} else {
+
+				default:
 					fmt.Println("Aucune commande ne correspond à votre saisie.")
 				}
 			} else {
 				fmt.Println("Il est nécessaire de saisir un argument à la suite de la commande dir.")
 			}
-		} else if os.Args[1] == "-help" {
-			// Liste de toutes les commandes disponibles
-			fmt.Println("C'est en cours")
-		} else if os.Args[1] == "file" {
+
+		case "file":
 			// Vérifie si le nombre d'argument est supérieur à 2
 			if len(os.Args) > 2 {
 
-				// CRUD
-				if os.Args[2] == "create" {
+				switch os.Args[2] {
+				case "create":
 					// Vérifie si le nombre d'argument est supérieur à 3
 					if len(os.Args) > 3 {
 						// Créer un fichier
@@ -70,7 +73,8 @@ func main() {
 					} else {
 						fmt.Println("Le chemin est vide.")
 					}
-				} else if os.Args[2] == "read" {
+
+				case "read":
 					// Vérifie si le nombre d'argument est supérieur à 3
 					if len(os.Args) > 3 {
 						// Lire le fichier
@@ -78,7 +82,8 @@ func main() {
 					} else {
 						fmt.Println("Le chemin est vide.")
 					}
-				} else if os.Args[2] == "updatename" {
+
+				case "updatename":
 					// Vérifie si le nombre d'argument est supérieur à 4
 					if len(os.Args) > 4 {
 						// Met à jour le nom du dossier
@@ -86,7 +91,8 @@ func main() {
 					} else {
 						fmt.Println("Le chemin est vide.")
 					}
-				} else if os.Args[2] == "updatetext" {
+
+				case "updatetext":
 					// Vérifie si le nombre d'argument est supérieur à 4
 					if len(os.Args) > 4 {
 						// Met à jour le nom du dossier
@@ -94,7 +100,8 @@ func main() {
 					} else {
 						fmt.Println("Le chemin est vide.")
 					}
-				} else if os.Args[2] == "delete" {
+
+				case "delete":
 					// Vérifie si le nombre d'argument est supérieur à 3
 					if len(os.Args) > 3 {
 						// Delete un dossier
@@ -102,12 +109,20 @@ func main() {
 					} else {
 						fmt.Println("Le chemin est vide.")
 					}
-				} else {
+
+				default:
 					fmt.Println("Aucune commande ne correspond à votre saisie.")
 				}
+
 			} else {
-				fmt.Println("Il est nécessaire de saisir un argument à la suite de la commande dir.")
+				fmt.Println("Il est nécessaire de saisir un argument à la suite de la commande file.")
 			}
+
+		case "-help":
+			// Liste de toutes les commandes disponibles
+			fmt.Println("C'est en cours")
+		default:
+			fmt.Println("Aucune commande ne correspond à votre saisie.")
 		}
 	} else {
 		fmt.Println("Exécutez la commande \"-help\" si vous n'êtes pas familier avec les instructions disponibles.")
