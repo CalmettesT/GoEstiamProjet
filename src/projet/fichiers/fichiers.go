786ef7444403a0887bcbf15b0a6cc9b955aefc94
CalmettesT <78858536+CalmettesT@users.noreply.github.com>
2024-01-25 17:24:24 +0100

    ADD : test unitaire et gestion d'erreur fichier.go. Manque encore main.go

--- src/projet/fichiers/fichiers.go
+++ src/projet/fichiers/fichiers.go
@@ -1,62 +1,78 @@
 package fichiers
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
 
-func CreateFile(name string) {
+func CreateFile(name string, text string) error {
 	filePath := "C:\\GoEstiamProjet\\src\\data\\" + name
+
+	// Vérifie si le fichier existe déjà
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		file, err := os.Create(filePath)
 		if err != nil {
-			fmt.Println("Impossible de créer le fichier:", err)
-			return
+			return errors.New("impossible de créer le fichier")
 		}
 		defer file.Close()
-		fmt.Println("Fichier créé:", filePath)
+
+		// Si du texte est fourni, écrivez-le dans le fichier
+		if text != "" {
+			errorWrite := os.WriteFile(filePath, []byte(text), 0644)
+			if errorWrite != nil {
+				return errors.New("le fichier ne s'est pas remplie")
+			}
+		}
+
+		fmt.Println("Fichier créé")
+		return nil
 	} else {
-		fmt.Println("Le fichier existe déjà.")
+		return errors.New("le fichier existe déjà")
 	}
 }
 
-func ReadFile(name string) {
+func ReadFile(name string) (string, error) {
 	filePath := "C:\\GoEstiamProjet\\src\\data\\" + name
 	data, err := os.ReadFile(filePath)
+
 	if err != nil {
-		fmt.Println("Impossible de lire le fichier:", err)
-		return
+		return "", errors.New("ce fichier n'existe pas")
+
 	}
 	fmt.Println("Contenu du fichier:", string(data))
+	return string(data), nil
 }
 
-func UpdateTextFile(name string, data string) {
+func UpdateTextFile(name string, data string) error {
 	filePath := "C:\\GoEstiamProjet\\src\\data\\" + name
 	err := os.WriteFile(filePath, []byte(data), 0644)
 	if err != nil {
-		fmt.Println("Impossible de mettre à jour le fichier:", err)
-		return
+		return errors.New("impossible de mettre à jour le fichier")
 	}
 	fmt.Println("Fichier mis à jour.")
+	return nil
 }
 
-func UpdateNameFile(oldName string, newName string) {
+func UpdateNameFile(oldName string, newName string) error {
 	oldPath := "C:\\GoEstiamProjet\\src\\data\\" + oldName
 	newPath := "C:\\GoEstiamProjet\\src\\data\\" + newName
 	err := os.Rename(oldPath, newPath)
 	if err != nil {
-		fmt.Println("Impossible de mettre à jour le nom fichier:", err)
-		return
+		return errors.New("impossible de mettre à jour le nom fichier")
+
 	}
 	fmt.Println("Nom fichier mis à jour.")
+	return nil
 }
 
-func DeleteFile(name string) {
+func DeleteFile(name string) error {
 	filePath := "C:\\GoEstiamProjet\\src\\data\\" + name
 	err := os.Remove(filePath)
 	if err != nil {
-		fmt.Println("Impossible de supprimer le fichier:", err)
-		return
+		return errors.New("impossible de supprimer le fichier")
+
 	}
 	fmt.Println("Fichier supprimé.")
+	return nil
 }
--- src/projet/fichiers/fichiers_test.go
+++ src/projet/fichiers/fichiers_test.go
@@ -1,3 +1,184 @@
 package fichiers
 
-// import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestCreateFile_Success(t *testing.T) {
+	// Nom du fichier pour le test
+	testFileName := "TestCreateFile_Success.txt"
+	textTest := "test"
+
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + testFileName
+
+	// Appel de la fonction CreateFile
+	CreateFile(testFileName, textTest)
+
+	// Vérification de l'existence du fichier
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		t.Errorf("Fonction de création de fichier ne marche pas. %s", testFileName)
+	}
+
+	// Nettoyage: Supprimer le fichier après le test
+	os.Remove(filePath)
+}
+
+func TestCreateFile_FileExist(t *testing.T) {
+	// Nom du fichier pour le test
+	testFileName := "TestCreateFile_FileExist.txt"
+	textTest := "test"
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + testFileName
+	file, _ := os.Create(filePath)
+	file.Close()
+
+	// Appel de la fonction CreateFile
+	CreateFile(testFileName, textTest)
+
+	// Vérification de l'existence du fichier
+	if _, err2 := os.Stat(filePath); err2 == nil {
+		t.Error("Le Fichier existe déjà")
+	}
+}
+
+func TestReadFile_Success(t *testing.T) {
+	// Nom du fichier pour le test
+	testFileName := "TestReadFile_Success.txt"
+	textTest := "test"
+
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + testFileName
+	file, _ := os.Create(filePath)
+	file.Close()
+
+	//Ecriture dans le fichier
+	os.WriteFile(filePath, []byte(textTest), 0644)
+
+	// Appel de la fonction ReadFile
+	data, _ := ReadFile(testFileName)
+
+	// Vérification de l'existence du fichier
+	if data != textTest {
+		t.Error("La fonction read ne marche pas")
+	}
+
+	// Nettoyage: Supprimer le fichier après le test
+	os.Remove(filePath)
+}
+
+func TestReadFile_FileNotExist(t *testing.T) {
+	// Nom du fichier pour le test
+	testFileName := "TestReadFile_FileNotExist.txt"
+
+	// Appel de la fonction ReadFile
+	_, err := ReadFile(testFileName)
+
+	// Vérification de l'existence du fichier
+	if err != nil {
+		t.Error("erreur :", err)
+	}
+}
+
+func TestUpdateTextFile_Success(t *testing.T) {
+	// Nom du fichier pour le test
+	testFileName := "TestUpdateTextFile_Success.txt"
+	textTest := "test"
+
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + testFileName
+	file, _ := os.Create(filePath)
+	file.Close()
+
+	//Ecriture dans le fichier
+	UpdateTextFile(testFileName, textTest)
+
+	data, _ := os.ReadFile(filePath)
+
+	// Vérification de l'existence du fichier
+	if string(data) != textTest {
+		t.Error("La fonction updateTextFile ne fonctionne pas")
+	}
+
+	// Nettoyage: Supprimer le fichier après le test
+	os.Remove(filePath)
+}
+
+func TestUpdateTextFile_FileNotExist(t *testing.T) {
+	// Nom du fichier pour le test
+	testFileName := "TestUpdateTextFile_FileNotExist.txt"
+	textTest := "test"
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + testFileName
+
+	//ecriture data
+	UpdateTextFile(testFileName, textTest)
+
+	// Appel de la fonction ReadFile
+	data, _ := os.ReadFile(filePath)
+
+	// Vérification de l'existence du fichier
+	if string(data) != textTest {
+		t.Error("Le fichier n'existe pas")
+	}
+}
+
+func TestUpdateNameFile_Success(t *testing.T) {
+	// Nom du fichier pour le test
+	oldTestFileName := "TestUpdateNameFile_Success.txt"
+	newNameTest := "newTestFile.txt"
+
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + oldTestFileName
+	file, _ := os.Create(filePath)
+	file.Close()
+
+	//Ecriture dans le fichier
+	UpdateNameFile(oldTestFileName, newNameTest)
+	newfilePath := "C:\\GoEstiamProjet\\src\\data\\" + newNameTest
+
+	// Vérification de l'existence du fichier
+	if _, err := os.Stat(newfilePath); os.IsNotExist(err) {
+		t.Error("La fonction updateNameFile ne fonctionne pas")
+	} else {
+		fmt.Println("update name marche")
+	}
+
+	// Nettoyage: Supprimer le fichier après le test
+	os.Remove(filePath)
+	os.Remove(newfilePath)
+}
+
+func TestDeleteFile_Success(t *testing.T) {
+	// Nom du fichier pour le test
+	testFileName := "TestDeleteFile_Success.txt"
+
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + testFileName
+	file, _ := os.Create(filePath)
+	file.Close()
+
+	// Appel de la fonction DeleteFile
+	DeleteFile(testFileName)
+
+	// Vérification de l'existence du fichier
+	if _, err2 := os.Stat(filePath); err2 == nil {
+		t.Errorf("Fonction de supression de fichier ne marche pas. %s", testFileName)
+	}
+
+	// Nettoyage: Supprimer le fichier après le test
+	os.Remove(filePath)
+}
+
+func TestDeleteFile_NotExist(t *testing.T) {
+	// Nom du fichier pour le test
+	testFileName := "TestDeleteFile_NotExist.txt"
+
+	filePath := "C:\\GoEstiamProjet\\src\\data\\" + testFileName
+
+	// Appel de la fonction DeleteFile
+	DeleteFile(testFileName)
+
+	// Vérification de l'existence du fichier
+	if _, err2 := os.Stat(filePath); err2 == nil {
+		t.Errorf("Le fichier n'existe pas donc pas de supression %s", testFileName)
+	}
+
+	// Nettoyage: Supprimer le fichier après le test
+	os.Remove(filePath)
+}
